1b201f60bb87e6ee959a886429ce6b6c47b10463
Dr. Diether Knof <dknof@gmx.de>
2015-02-05 21:28:45 +0100

    javatron mit integriert

--- src/erstelle_pdf.sh
+++ src/erstelle_pdf.sh
@@ -10,11 +10,9 @@ rm ./tronbot
 echo "Kompiliere tronbot…"
 make -j1 || exit
 
-mkdir ../Ergebnisse.pdf
-for i in 1; do 
-  for f in ../freiesmagazin-2014-10-contest/results_turn$i/*.txt; do
-    echo "$i -- "$(basename "$f")
-    ./tronbot <"$f" >/dev/null
-    mv Spielraster.pdf ../Ergebnisse.pdf/$(basename "$f" .txt)-$i.pdf
-  done
+mkdir -p ../Ergebnisse.pdf
+for f in ../freiesmagazin-2014-10-contest/results/*.txt; do
+  echo $(basename "$f")
+  ./tronbot <"$f" >/dev/null
+  mv Spielraster.pdf ../Ergebnisse.pdf/$(basename "$f" .txt).pdf
 done
